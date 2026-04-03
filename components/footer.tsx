c918b2db107f21a1e9f3006d6470bead8396ddb7
lamvdn-hub <lam.vudongnguyen@gmail.com>
2026-04-03 23:06:03 +0200

    Updated footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -102,7 +102,7 @@ export function Footer() {
                 {t.footer.messageFacebook}
               </a>
               <a
-                href="https://zalo.me/[number]"
+                href="https://zalo.me/g/xegehi407"
                 target="_blank"
                 rel="noopener noreferrer"
                 style={socialButtonStyle}
